29d7a5d31ca334f199e3f6251b766428b0ce872c
lar9482 <73131102+lar9482@users.noreply.github.com>
2021-06-09 01:19:54 -0500

    Update Model.py
    1. Removed plot comparison and display_statistics.
        These will be placed in graph.py
    2. graph_results now calls an object that handles the matplotlib details.

--- TURC/Model.py
+++ TURC/Model.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 import math
+import graph
+from graph import *
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -69,72 +71,33 @@ class Model:
 			self.index_predicted = self.Model.predict(self.index_test)
 			print(self.index_predicted)
 
+			#Calculating statistics on this computation
+			self.calculate_statistics()
+
 		elif (testSet == 'fail'):
 			print(self.fail_test)
 			print('Predicted output on fail_test:')
 			self.fail_predicted = self.Model.predict(self.fail_test)
 			print(self.fail_predicted)
 
-	def plot_comparison(self, test_set, predict_set, set_name, trainingModel, penality, maxIteration, solver):
-		length = len(predict_set)
+			self.calculate_RMSE(self.fail_test, self.fail_predicted)
+			self.calculate_F_Score(self.fail_test, self.fail_predicted)
+			self.calculate_AIC()
 
-		x1 = [i for i in range(0, length)]
-		y1 = [test_set[i][0] for i in range(0, length)]
-
-		x2 = [i for i in range(0, length)]
-		y2 = [predict_set[i] for i in range(0, length)]
-
-		#Creates plots for the test and predicted data
-		testContinuous = plt.plot(x1, y1, label = 'test_data', linestyle = 'solid')
-		predictedContinuous = plt.plot(x2, y2, label = 'predicted_data', linestyle = '--')
-		testDiscrete = plt.plot(x1, y1, "oc") #plots discrete values
-		predictDiscrete = plt.plot(x2, y2, "or")
-		#title = 'DataSet: ' + set_name + ', Model: ' + trainingModel + ', penality: ' + penality + ', maxIteration: ' + str(maxIteration)+', Algorithm: ' + solver
-		title = set_name+ ' ' +trainingModel+ ' ' +penality+ ' ' +str(maxIteration)+ ' ' +solver
-		#Creates the title and labels
-		plt.xlabel('index')
-		plt.ylabel('fail_times')
-		plt.title(title)
-
-		#Creates the legends of the lines
-		testLegend = plt.legend(handles = testContinuous, loc='upper right')
-		predictLegend = plt.legend(handles = predictedContinuous, loc='lower right')
-		plt.gca().add_artist(testLegend)
-		plt.gca().add_artist(predictLegend)
-
-		#Displays a table with the RMSE, F-Score, and AIC.
-		self.display_statistics()
-		plt.savefig(title)
-		
-	def display_statistics(self):
-		self.calculate_RMSE(self.fail_test, self.fail_predicted)
-		self.calculate_F_Score(self.fail_test, self.fail_predicted)
-		self.calculate_AIC()
-		colWidth = 1
-		rowHeight = 2.5
-		fontSize = 10
-
-		CellText = [['RMSE', self.RMSE], ['F-Score', self.F_Score], ['AIC', self.AIC]]
-		Loc = 'bottom'
-		table = plt.table(cellText = CellText, loc = Loc)
-		table.scale(colWidth, rowHeight)
-		table.auto_set_font_size(False)
-		table.set_fontsize(fontSize)
-		#Makes room for the table
-		plt.subplots_adjust(left=0.1, bottom=0.3)
-
-	def graph_results(self, test_set, set_name, trainingModel, penality, maxIteration, solver):
-		if (test_set == 'index'):
-			self.plot_comparison(self.index_test, self.index_predicted, set_name, trainingModel, penality, maxIteration, solver)
-		elif(test_set == 'fail'):
-			self.plot_comparison(self.fail_test, self.fail_predicted, set_name, trainingModel, penality, maxIteration, solver)
+	def graph_results(self, set_name, trainingModel, penality, maxIteration, solver):
+		try:
+			Graph = graph(self.fail_test, self.fail_predicted, set_name, trainingModel, penality, maxIteration, solver, self.RMSE, self.F_Score, self.AIC)
+			Graph.save_graph()
+			Graph.clear_graph()
+		except:
+			print('Params are not right')
 
 	def calculate_RMSE(self, test_set, predicted_set):
 		self.RMSE = mean_squared_error(test_set.reshape(1, -1)[0], predicted_set, squared = False)
 
 	def calculate_F_Score(self, test_set, predicted_set):
 		
-		self.F_Score = f1_score(test_set.reshape(1, -1)[0], predicted_set, average = None, zero_division = 1)
+		self.F_Score = f1_score(test_set.reshape(1, -1)[0], predicted_set, average = 'weighted', zero_division = 0)
 		if (isinstance(self.F_Score, (float)) is False):
 			self.F_Score = 'Zero_Division detected'
 
@@ -144,6 +107,10 @@ class Model:
 		else:
 			self.AIC = 2*(numParams - math.log(self.RMSE))
 
+	def calculate_statistics(self, test_set, predicted_set):
+		self.calculate_RMSE(test_set, predicted_set)
+		self.calculate_F_Score(test_set, predicted_set)
+		self.calculate_AIC()
 		
 
 dataSets = ['SYS1', 'SYS2', 'SYS3', 'CSR1', 'CSR2']
@@ -154,15 +121,15 @@ for i in dataSets:
 	model = Model('model_data.xlsx', i)
 	for j in algoNames:
 		for l in penalities:
-			max_iteration = 2000
+			max_iteration = 1000
 			try:
 				model.train_logistic_regression(l, max_iteration, j)
 			except:
 				print('Failed to fit %s using %s' % (l, j))
 				continue
 
-			while max_iteration <= 20000:
+			while max_iteration <= 5000:
 				model.train_logistic_regression(l, max_iteration, j)
 				model.predict_logistic_regression('fail')
-				model.graph_results('fail', i, 'Logistic-Regression', l, max_iteration, j)
-				max_iteration = max_iteration + 2000
+				model.graph_results(i, 'Logistic-Regression', l, max_iteration, j)
+				max_iteration = max_iteration + 1000

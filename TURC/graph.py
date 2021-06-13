d4e748cb75ab02189bd87e2867304c3232d387c4
lar9482 <73131102+lar9482@users.noreply.github.com>
2021-06-13 18:11:01 -0500

    Versatility Update
    1. Deleted statistics table.
        Statistics will be moved into the excel files.

--- TURC/graph.py
+++ TURC/graph.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 
 class graph:
-	def __init__(self, test_set, predict_set, set_name, trainingModel, penality, maxIteration, solver, RMSE, F_Score, AIC):
-		self.title = set_name+ ' ' +trainingModel+ ' ' +penality+ ' ' +str(maxIteration)+ ' ' +solver
+	def __init__(self, test_set, predict_set, modelName, names):
+		self.title = modelName
+		self.title = self.title + names
+		
 		self.time = [i for i in range(0, len(predict_set))]
 		self.test = [test_set[i][0] for i in range(0, len(predict_set))]
 		self.predict = [predict_set[i] for i in range(0, len(predict_set))]
-
-		self.RMSE = RMSE
-		self.F_Score = F_Score
-		self.AIC = AIC
 
 	def build_graph(self):
 		#Creates plots for the test and predicted data
@@ -19,7 +17,7 @@ class graph:
 		predictDiscrete = plt.plot(self.time, self.predict, "or")
 		
 		#Creates the title and labels
-		plt.xlabel('index')
+		# plt.xlabel('index')
 		plt.ylabel('fail_times')
 		plt.title(self.title)
 
@@ -29,25 +27,9 @@ class graph:
 		plt.gca().add_artist(testLegend)
 		plt.gca().add_artist(predictLegend)
 
-		#Displays a table with the RMSE, F-Score, and AIC.
-		
-	def build_statistics_table(self):
-		colWidth = 1
-		rowHeight = 2.5
-		fontSize = 10
-
-		CellText = [['RMSE', self.RMSE], ['F-Score', self.F_Score], ['AIC', self.AIC]]
-		Loc = 'bottom'
-		table = plt.table(cellText = CellText, loc = Loc)
-		table.scale(colWidth, rowHeight)
-		table.auto_set_font_size(False)
-		table.set_fontsize(fontSize)
-		#Makes room for the table
-		plt.subplots_adjust(left=0.1, bottom=0.3)
 
 	def save_graph(self):
 		self.build_graph()
-		self.build_statistics_table()
 		plt.savefig(self.title)
 
 	def clear_graph(self):

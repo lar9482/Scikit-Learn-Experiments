9d8c28cf9ea8560dfe0d1754158e56490577b847
lar9482 <73131102+lar9482@users.noreply.github.com>
2021-06-15 13:49:43 -0500

    Update Model.py

--- TURC/Model.py
+++ TURC/Model.py
@@ -17,6 +17,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
 
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
@@ -76,24 +77,25 @@ class Model:
 		model_Name = ''
 		if (modelName == 'LR'):
 			self.Model = LogisticRegression(penalty = coFactors, max_iter = maxIterations, solver = algoName)
-			self.Model.fit(self.index_train, self.fail_train)
 			model_Name = 'Logistic-Regression '
 
 		elif(modelName == 'SVR'):
 			self.Model = SVR(kernel = algoName, max_iter = maxIterations)
-			self.Model.fit(self.index_train, self.fail_train)
 			model_Name = 'Support-Vector-Machine '
 
 		elif (modelName == 'DTR'):
-			self.Model = DecisionTreeRegressor()
-			self.Model.fit(self.index_train, self.fail_train)
+			self.Model = DecisionTreeRegressor(criterion = algoName, max_depth = maxIterations)
 			model_Name = 'Decision-Tree '
 
 		elif (modelName == 'RFR'):
-			self.Model = RandomForestRegressor(n_estimators = maxIterations)
-			self.Model.fit(self.index_train, self.fail_train)
+			self.Model = RandomForestRegressor(n_estimators = maxIterations, criterion = algoName, max_depth = maxIterations)
 			model_Name = 'Random-Forest '
 
+		elif (modelName == 'MLPR'):
+			self.Model = MLPRegressor(hidden_layer_sizes = maxIterations, activation = coFactors, solver = algoName)
+			model_Name = 'Mult-Layer-Perceptron '
+
+		self.Model.fit(self.index_train, self.fail_train)
 		return model_Name
 
 
@@ -116,7 +118,6 @@ class Model:
 		self.RMSE = mean_squared_error(test_set.reshape(1, -1)[0], predicted_set, squared = False)
 
 	def calculate_F_Score(self, test_set, predicted_set):
-		
 		self.F_Score = f1_score(test_set.reshape(1, -1)[0], predicted_set, average = 'weighted', zero_division = 0)
 		if (isinstance(self.F_Score, (float)) is False):
 			self.F_Score = 'Zero_Division detected'
@@ -129,7 +130,7 @@ class Model:
 
 	def calculate_statistics(self):
 		self.calculate_RMSE(self.fail_test, self.fail_predicted)
-		self.calculate_F_Score(self.fail_test, self.fail_predicted)
+		#self.calculate_F_Score(self.fail_test, self.fail_predicted)
 		self.calculate_AIC()
 
 	def save_data(self, dataSetName, sheetName):
@@ -150,7 +151,7 @@ class Model:
 	#NOTE:
 	#covariant factors(coFactors) can be metrics like 'penalities' etc.
 	@staticmethod
-	def runTests(dataSets, modelName, algoNames, coFactors = []):
+	def runTestsWithCofactors(dataSets, modelName, algoNames, coFactors):
 		for data_Sets in dataSets:
 			# model = Model('model_data.xlsx', data_Sets)
 			#DataSet, algoName, coFactors, max_iteration
@@ -182,10 +183,61 @@ class Model:
 						print('Processed' + ' '.join([i for i in names]))
 						names = [data_Sets, algorithms, variant, str(max_iteration)]
 
+	def runTests(dataSets, modelName, algoNames):
+		for data_Sets in dataSets:
+			# model = Model('model_data.xlsx', data_Sets)
+			#DataSet, algoName, coFactors, max_iteration
 
-dataSets = ['SYS1', 'SYS2', 'SYS3', 'CSR1', 'CSR2']
-optimizeAlgorithms = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
-penalities = ['l1', 'l2', 'elasticnet', 'none']
-modelName = 'LR'
+			for algorithms in algoNames:
+				
+				max_iteration = 1000
+				model_Name = ''
+				names = [data_Sets, algorithms]
+				directory = DataSave.createDirectoryPath(names)
+				os.makedirs(directory)
+					
+				model = Model('model_data.xlsx', data_Sets, directory)
+				names.append(str(max_iteration))
+				try:
+					model_Name = model.train_model(modelName, algorithms, max_iteration)
+				except:
+					print('Failed to fit %s' % (algorithms))
+					continue
+					
+				while max_iteration <= 20000:
+					model_Name = model.train_model(modelName, algorithms, max_iteration)
+					model.predict_model('fail')
+					model.calculate_statistics()
+						
+					model.graph_results(modelName = model_Name, names = ' '.join([i for i in names]), path = directory)
+					model.save_data(dataSetName = data_Sets, sheetName = ' '.join([i for i in names]))
+					max_iteration = max_iteration + 1000
+					print('Processed' + ' '.join([i for i in names]))
+					names = [data_Sets, algorithms, str(max_iteration)]
 
-Model.runTests(dataSets, modelName, optimizeAlgorithms, penalities)
+# dataSets = ['SYS1', 'SYS2', 'SYS3', 'CSR1', 'CSR2']
+# dataSets = ['SYS1']
+# optimizeAlgorithms = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+# penalities = ['l1', 'l2', 'elasticnet', 'none']
+# modelName = 'LR'
+
+# dataSets = ['SYS1']
+# kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+# modelName = 'SVR'
+
+# dataSets = ['SYS1']
+# criterion = ['mse', 'friedman_mse', 'mae', 'poisson']
+# modelName = 'DTR'
+
+# dataSets = ['SYS1']
+# criterion = ['mse', 'mae']
+# modelName = 'RFR'
+
+
+dataSets = ['SYS1']
+activation = ['identity', 'logistic', 'tanh', 'relu']
+solver = ['lbfgs', 'sgd', 'adam']
+
+modelName = 'MLPR'
+
+Model.runTestsWithCofactors(dataSets, modelName, solver, activation)

c8390758a57ef1dd1d56ef1c4b9af737837b6dc7
lar9482 <73131102+lar9482@users.noreply.github.com>
2021-06-23 15:25:08 -0500

    Metrics are saved into a universal file.
    1. statDictionary is now present in runTests and runTestsWithCofactors.
        This is keep track of the metrics based on algorithm and/or variants. 
        The static method update_stat_file handles the I/O operations.
    2. Known Issues:
        The entries in statDictionary must all be the same length.
        This doesn't allow for exceptions(failed to fit model using algorithm i or variant j, etc.)
        I'll see if I can fix this soon.

--- TURC/Model.py
+++ TURC/Model.py
@@ -19,14 +19,14 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 
-from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.metrics import f1_score
 from sklearn.metrics import mean_squared_error
+from sklearn.exceptions import ConvergenceWarning
 
 class Model:
-	def __init__(self, dataSetName, sheetName, path, testSize = 0.2):
+	def __init__(self, dataSetName, sheetName, path, algoNames, testSize = 0.15):
 		self.dataSet = pd.read_excel(dataSetName, sheetName)
 		self.fail_indexes = np.array(self.dataSet['FN'])
 		self.fail_times = np.array(self.dataSet['FT'])
@@ -43,8 +43,8 @@ class Model:
 		self.Model = None
 
 		self.RMSE = 0.00
-		self.F_Score = 0.00
 		self.AIC = 0.00
+
 
 	#This function cleans up the data
 	#Essentially, it scales the data to have zero mean and variance
@@ -60,7 +60,7 @@ class Model:
 
 		#The fail_times data is encoded as floating points(not present in the SYS1 file)
 		#This encodes the fail_times data as ints
-		label_encoder = preprocessing.LabelEncoder()
+		label_encoder = LabelEncoder()
 		self.fail_indexes = label_encoder.fit_transform(self.fail_indexes)
 		self.fail_times = label_encoder.fit_transform(self.fail_times)
 
@@ -71,7 +71,6 @@ class Model:
 		self.index_train = self.index_train.reshape(-1, 1)
 		self.index_test = self.index_test.reshape(-1, 1)
 		self.fail_test = self.fail_test.reshape(-1, 1)
-
 
 	def train_model(self, modelName, algoName, maxIterations = 1000, coFactors = None):
 		model_Name = ''
@@ -98,7 +97,6 @@ class Model:
 		self.Model.fit(self.index_train, self.fail_train)
 		return model_Name
 
-
 	def predict_model(self, testSet):
 		if (testSet == 'index'):	
 			self.index_predicted = self.Model.predict(self.index_test)
@@ -115,12 +113,9 @@ class Model:
 			traceback.print_exc()
 
 	def calculate_RMSE(self, test_set, predicted_set):
+		# print(test_set.reshape(1, -1)[0])
+		# print(predicted_set)
 		self.RMSE = mean_squared_error(test_set.reshape(1, -1)[0], predicted_set, squared = False)
-
-	def calculate_F_Score(self, test_set, predicted_set):
-		self.F_Score = f1_score(test_set.reshape(1, -1)[0], predicted_set, average = 'weighted', zero_division = 0)
-		if (isinstance(self.F_Score, (float)) is False):
-			self.F_Score = 'Zero_Division detected'
 
 	def calculate_AIC(self, numParams = 4):
 		if (self.RMSE == 0):
@@ -130,32 +125,41 @@ class Model:
 
 	def calculate_statistics(self):
 		self.calculate_RMSE(self.fail_test, self.fail_predicted)
-		#self.calculate_F_Score(self.fail_test, self.fail_predicted)
 		self.calculate_AIC()
 
-	def save_data(self, dataSetName, sheetName):
+	#This function creates a separate excel file that saves the test and predicted data,
+	#into the same directory as self.excelWriter.
+	#Please note that the directories are named based on the current algorithm, data set, and/or co-variant.
+	def save_data(self, sheetName):
 		df = pd.DataFrame({
 			"FT": self.fail_test.reshape(1, -1)[0],
-			"FP": self.fail_predicted,
-			"RMSE": self.RMSE,
-			"F_Score": self.F_Score,
-			"AIC": self.AIC
+			"FP": self.fail_predicted
 			})
 
 		df.to_excel(self.excelWriter, sheet_name = sheetName, index = False)
-		
 		self.excelWriter.save()
 	
-	def close_writer(self):
-		self.excelWriter.close()
-	#NOTE:
+	#These are helper functions that are used to create custom directories
+	def construct_stat_table(self, max_iter):
+		return [max_iter, self.RMSE, self.AIC]
+
+	def construct_covariant_stat_table(self, max_iter, coFactor):
+		return [coFactor, max_iter, self.RMSE, self.AIC]
+	
+	
+	@staticmethod
+	def update_stat_file(sheetName, statDictionary, excelWriter):
+		print(statDictionary)
+		df = pd.DataFrame(statDictionary)
+		df.to_excel(excelWriter, sheet_name = sheetName, index = False)
+		excelWriter.save()
+
+	# #NOTE:
 	#covariant factors(coFactors) can be metrics like 'penalities' etc.
 	@staticmethod
-	def runTestsWithCofactors(dataSets, modelName, algoNames, coFactors):
+	def runTestsWithCofactors(dataSets, modelName, algoNames, coFactors, excelWriter):
 		for data_Sets in dataSets:
-			# model = Model('model_data.xlsx', data_Sets)
-			#DataSet, algoName, coFactors, max_iteration
-
+			statDictionary = {i : [' '] for i in algoNames}
 			for algorithms in algoNames:
 				for variant in coFactors:
 					max_iteration = 1000
@@ -164,80 +168,119 @@ class Model:
 					directory = DataSave.createDirectoryPath(names)
 					os.makedirs(directory)
 					
-					model = Model('model_data.xlsx', data_Sets, directory)
+					model = Model('model_data.xlsx', data_Sets, directory, algorithms)
 					names.append(str(max_iteration))
+
 					try:
 						model_Name = model.train_model(modelName, algorithms, max_iteration, variant)
 					except:
 						print('Failed to fit %s using %s' % (variant, algorithms))
+						statDictionary = statDictionary.pop(algorithm)
 						continue
 					
-					while max_iteration <= 20000:
-						model_Name = model.train_model(modelName, algorithms, max_iteration, variant)
+					while max_iteration <= 5000:
+						convergeFlag = 'ConvergeSuccess'
+						try:
+							model_Name = model.train_model(modelName, algorithms, max_iteration, variant)
+						except:
+							convergeFlag = 'ConvergeFail'
+
 						model.predict_model('fail')
 						model.calculate_statistics()
+
+						statsTable = model.construct_covariant_stat_table(variant, max_iteration)
+						statsTable.append(convergeFlag)
+						statsTable.append(' ')
 						
 						model.graph_results(modelName = model_Name, names = ' '.join([i for i in names]), path = directory)
-						model.save_data(dataSetName = data_Sets, sheetName = ' '.join([i for i in names]))
+						model.save_data(sheetName = ' '.join([i for i in names]))
+						
+						for i in statsTable:
+							statDictionary[algorithms].append(i)
+
 						max_iteration = max_iteration + 1000
+
 						print('Processed' + ' '.join([i for i in names]))
 						names = [data_Sets, algorithms, variant, str(max_iteration)]
 
-	def runTests(dataSets, modelName, algoNames):
+			Model.update_stat_file(modelName + ' ' + data_Sets, statDictionary, excelWriter)
+
+	@staticmethod
+	def runTests(dataSets, modelName, algoNames, excelWriter):
 		for data_Sets in dataSets:
-			# model = Model('model_data.xlsx', data_Sets)
-			#DataSet, algoName, coFactors, max_iteration
-
+			statDictionary = {i : [' '] for i in algoNames}
 			for algorithms in algoNames:
-				
-				max_iteration = 1000
-				model_Name = ''
-				names = [data_Sets, algorithms]
-				directory = DataSave.createDirectoryPath(names)
-				os.makedirs(directory)
+					max_iteration = 1000
+					model_Name = ''
+					names = [data_Sets, algorithms]
+					directory = DataSave.createDirectoryPath(names)
+					os.makedirs(directory)
 					
-				model = Model('model_data.xlsx', data_Sets, directory)
-				names.append(str(max_iteration))
-				try:
-					model_Name = model.train_model(modelName, algorithms, max_iteration)
-				except:
-					print('Failed to fit %s' % (algorithms))
-					continue
-					
-				while max_iteration <= 20000:
-					model_Name = model.train_model(modelName, algorithms, max_iteration)
-					model.predict_model('fail')
-					model.calculate_statistics()
-						
-					model.graph_results(modelName = model_Name, names = ' '.join([i for i in names]), path = directory)
-					model.save_data(dataSetName = data_Sets, sheetName = ' '.join([i for i in names]))
-					max_iteration = max_iteration + 1000
-					print('Processed' + ' '.join([i for i in names]))
-					names = [data_Sets, algorithms, str(max_iteration)]
+					model = Model('model_data.xlsx', data_Sets, directory, algorithms)
+					names.append(str(max_iteration))
 
-# dataSets = ['SYS1', 'SYS2', 'SYS3', 'CSR1', 'CSR2']
+					try:
+						model_Name = model.train_model(modelName, algorithms, max_iteration)
+					except:
+						print('Failed to fit %s' % (algorithms))
+						#remove algorithm name from statDictionary soon.
+						
+						continue
+					
+					while max_iteration <= 5000:
+						convergeFlag = 'ConvergeSuccess'
+						try:
+							model_Name = model.train_model(modelName, algorithms, max_iteration)
+						except:
+							convergeFlag = 'ConvergeFail'
+
+						model.predict_model('fail')
+						model.calculate_statistics()
+
+						statsTable = model.construct_stat_table(max_iteration)
+						statsTable.append(convergeFlag)
+						statsTable.append(' ')
+						
+						model.graph_results(modelName = model_Name, names = ' '.join([i for i in names]), path = directory)
+						model.save_data(sheetName = ' '.join([i for i in names]))
+						
+						for i in statsTable:
+							statDictionary[algorithms].append(i)
+
+						max_iteration = max_iteration + 1000
+
+						print('Processed' + ' '.join([i for i in names]))
+						names = [data_Sets, algorithms, str(max_iteration)]
+
+			Model.update_stat_file(sheetName = modelName + ' ' + data_Sets, statDictionary = statDictionary, excelWriter = excelWriter)	
+
+
+statWriter = pd.ExcelWriter(os.path.join(os.getcwd(), 'Stats') + '.xlsx')
+
 # dataSets = ['SYS1']
-# optimizeAlgorithms = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+# optimizeAlgorithms = ['newton-cg', 'lbfgs', 'sag', 'saga']
 # penalities = ['l1', 'l2', 'elasticnet', 'none']
 # modelName = 'LR'
+# Model.runTestsWithCofactors(dataSets, modelName, solver, activation, statWriter)
 
-# dataSets = ['SYS1']
-# kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
-# modelName = 'SVR'
+dataSets = ['SYS1']
+kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+modelName = 'SVR'
+Model.runTests(dataSets, modelName, kernels, statWriter)
 
 # dataSets = ['SYS1']
 # criterion = ['mse', 'friedman_mse', 'mae', 'poisson']
 # modelName = 'DTR'
+# Model.runTests(dataSets, modelName, criterion, statWriter)
 
 # dataSets = ['SYS1']
 # criterion = ['mse', 'mae']
 # modelName = 'RFR'
+# Model.runTests(dataSets, modelName, criterion, statWriter)
 
+# dataSets = ['SYS1']
+# activation = ['identity', 'logistic', 'tanh', 'relu']
+# solver = ['lbfgs', 'adam']
 
-dataSets = ['SYS1']
-activation = ['identity', 'logistic', 'tanh', 'relu']
-solver = ['lbfgs', 'sgd', 'adam']
-
-modelName = 'MLPR'
-
-Model.runTestsWithCofactors(dataSets, modelName, solver, activation)
+# modelName = 'MLPR'
+# Model.runTestsWithCofactors(dataSets, modelName, solver, activation, statWriter)
